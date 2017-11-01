cb23f2222069ded651b400d11a7cfd4ec8b70024
ivanu <ivanuet.lozada@gmail.com>
2017-11-01 12:35:21 -0600

    Inicio de proyecto

---
+++ src/Nodo.java
@@ -0,0 +1,19 @@
+public class Nodo {//clase que define los elementos que deve tener un nodo en la lista
+    public int valor; //variable en la que se guardara el valor
+    private Nodo siguiente;//variable para enlazar los nodos
+
+    public void Nodo (){ //constructor que inicializara el valor de las variables
+        this.valor=0;
+        this.siguiente = null;
+    }
+//metodos get y set para los atributos
+    public int valor () {
+        return valor;
+    }
+
+    public void SetValor (int )
+
+
+
+
+}
